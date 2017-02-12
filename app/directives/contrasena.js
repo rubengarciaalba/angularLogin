de0ffd3d42175f6684afb336c5544c57d3284c3f
Rubén Garcia <Rubn Garcia>
2017-02-12 11:58:39 +0100

    directive in typescript

--- app/directives/contrasena.js
+++ app/directives/contrasena.js
@@ -1,30 +1,15 @@
-"use strict";
-var passwordmatchProvider = function (directivesPath) {
-    return {
-        restrict: "AE",
-        replace: true,
-        require: ["passwordmatch"],
-        $scope: {
-            data: "=data",
-            model: "=ngModel"
-        },
-        controller: PasswordMatchController2.id,
-        // controllerAs: "vm",
-        link: function ($scope, element, attrs, controller) {
-            $scope.element = element;
-            var checker = function () {
-                //get the value of the first password
-                var e1 = $scope.$eval(attrs.ngModel);
-                //get the value of the other password  
-                var e2 = $scope.$eval(attrs.passwordMatch);
-                if (e2 != null)
-                    return e1 == e2;
-            };
-            $scope.$watch(checker, function (oldValue, newValue) {
-                controller[0].displayMessage();
-            });
-        }
+/// <reference path="../../node_modules/@types/angular/index.d.ts" />
+"user strict";
+var PasswordMatch = (function () {
+    function PasswordMatch() {
+        this.restrict = 'EA';
+        console.log("assssdlfjalsdjflsjlfk");
+    }
+    PasswordMatch.instance = function () {
+        return new PasswordMatch();
     };
-};
-angular.module("myApp").directive("passwordmatch", ["directivesPath", passwordmatchProvider]);
+    return PasswordMatch;
+}());
+PasswordMatch.$inject = [''];
+angular.module('myApp').directive('passwordMatch', PasswordMatch.instance);
 //# sourceMappingURL=contrasena.js.map
--- app/directives/contrasena.ts
+++ app/directives/contrasena.ts
@@ -1,39 +1,19 @@
-"use strict";
+/// <reference path="../../node_modules/@types/angular/index.d.ts" />
 
-interface IDirectiveScope {
-    element?: ng.IAugmentedJQuery;
+"user strict"
+
+class PasswordMatch implements ng.IDirective {
+  static $inject: Array<string> = [''];
+  constructor() {
+    console.log("assssdlfjalsdjflsjlfk");
+    //ToDo
+    // - Instance staff for the directive here
+  }
+
+  static instance(): ng.IDirective {
+    return new PasswordMatch();
+  }
+  restrict: string = 'EA';
 }
 
-let passwordmatchProvider = (directivesPath) => {
-    return {
-        restrict: "AE",
-        replace: true,
-        require: ["passwordmatch"],
-        $scope: {
-            data: "=data",
-            model: "=ngModel"
-          
-        },
-        controller: PasswordMatchController2.id,
-        // controllerAs: "vm",
-        
-        link: ($scope: any, element: ng.IAugmentedJQuery, attrs, controller: PasswordMatchController2) => {
-            $scope.element = element;  
-            var checker = (): boolean => {
- 
-                //get the value of the first password
-                var e1 = $scope.$eval(attrs.ngModel); 
- 
-                //get the value of the other password  
-                var e2 = $scope.$eval(attrs.passwordMatch);
-                if(e2!=null)
-                return e1 == e2;
-            };        
-            $scope.$watch(checker, function (oldValue,newValue) {
-                controller[0].displayMessage();            
-            });
-        }
-    };
-}
-
-angular.module("myApp").directive("passwordmatch", ["directivesPath", passwordmatchProvider]);
+angular.module('myApp').directive('passwordMatch', PasswordMatch.instance);
